3038989a118ec13f1f8f1aacb1a74a45d3f8d7be
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-30 18:41:40 +0530

    success page updating...

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@
 import React, { useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { db } from '../firebase.js';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
   const [extractedEmail, setExtractedEmail] = useState<string | null>(null);
-
+  const [loading, setLoading] = useState<boolean>(false);
   const search = useSearchParams();
+  const router = useRouter();
 
   const extractEmail = async () => {
     try {
@@ -15,10 +17,12 @@ export default function Home() {
 
       if (extractedEmail) {
         const doc = db.collection('attendees').doc(extractedEmail);
+        setLoading(true);
+
         await doc.update({ flag: true });
 
         // Since the update method doesn't return a value, you can assume success if it doesn't throw an error
-        alert('Success');
+        router.push('/success');
       } else {
         alert('Email not found');
       }
@@ -36,32 +40,32 @@ export default function Home() {
           className="md:w-1/2 md:h-1/2 w-auto h-auto object-cover my-12"
         />
       </div>
-      <div
-        className="bg-gradient-to-b from-yellow-900 to-yellow-600 
-      text-white font-sans flex flex-col justify-center items-center min-h-fit"
-      >
-        <div className="container mx-auto p-4 flex items-center justify-center flex-col">
-          <p className="text-3xl font-bold my-16 text-center font-mono">
-            Welcome to DevFest 2023!
-          </p>
-          <p className="text-2xl font-semibold my-6 text-center font-mono">
-            Please RSVP using the form below.
-          </p>
-        </div>
-
+      {!loading ? (
         <div
-          className="bg-white text-yellow-600 my-16 p-4 rounded-full hover:bg-white/80 cursor-pointer transition duration-200"
-          onClick={extractEmail}
+          className="bg-yellow-500 
+      text-white font-sans flex flex-col justify-center items-center min-h-fit"
         >
-          <h1 className="text-2xl font-bold px-4">
-            Confirm your participation
-          </h1>
-        </div>
-      </div>
+          <div className="container mx-auto p-4 flex items-center justify-center flex-col">
+            <p className="text-3xl font-bold my-16 text-center font-mono">
+              Welcome to DevFest 2023!
+            </p>
+            <p className="text-2xl font-semibold my-6 text-center font-mono">
+              Please RSVP using the form below.
+            </p>
+          </div>
 
-      {extractedEmail && (
-        <div className="text-center mt-4">
-          <p className="text-lg">Extracted Email: {extractedEmail}</p>
+          <div
+            className="bg-white text-yellow-600 my-16 p-4 rounded-full hover:bg-white/80 cursor-pointer transition duration-200"
+            onClick={extractEmail}
+          >
+            <h1 className="text-2xl font-bold px-4">
+              Confirm your participation
+            </h1>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-screen text-white">
+          Loading...
         </div>
       )}
     </section>
--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,7 +1,64 @@
+'use client';
+import React, { useState, useRef, useEffect } from 'react';
+import localFont from 'next/font/local';
+const myFont = localFont({ src: '../GoogleSans-Regular-v1.27.ttf' });
 
+export default function Page() {
+  const [width, setWidth] = useState(-1);
+  const [height, setHeight] = useState(-1);
+  const [x, setX] = useState(-1);
+  const [y, setY] = useState(-1);
 
-export default function page() {
+  const imageRef = useRef(null);
+
+  useEffect(() => {
+    if (imageRef.current) {
+      setWidth(imageRef.current.clientHeight * 0.6363636364);
+      setHeight(imageRef.current.clientHeight);
+      setX(imageRef.current.offsetLeft / 2);
+      console.log(
+        `Image width: ${width}, height: ${height}, Left: ${imageRef.current.offsetLeft}`
+      );
+    }
+  }, []);
+
+  let firstname = 'Sree Teja';
+  let lastname = 'Dusi';
+
+  if (firstname.length >= 12) {
+    const namesThird = firstname.split(' ');
+    if (namesThird.length >= 3) {
+      firstname = namesThird[0] + ' ' + namesThird[1];
+      lastname = namesThird[2];
+    } else {
+      firstname = namesThird[0];
+      lastname = namesThird[1];
+    }
+  }
+
+  const role = 'GITAM, Visakhapatnam';
   return (
-    <div></div>
-  )
+    <div className={myFont.className}>
+      <div className="relative flex justify-center items-center h-screen flex-col text-white">
+        <img
+          ref={imageRef}
+          src="/id.svg"
+          alt="ID card"
+          style={{
+            height: `100vh`,
+          }}
+        />
+
+        <div
+          className={`absolute top-0 left- w-20 bg-white h-20 right-0 bottom-0`}
+        >
+          <p className="text-3xl font-extrabold text-gray-900 ">{firstname}</p>
+          <p className="text-3xl font-bold text-gray-900 ">{lastname}</p>
+          <p className="text-md text-gray-600">{role}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+// w-[${width}] h-[${height}]
