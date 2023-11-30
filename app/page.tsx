9b7560f4b81a6aa6e3040c5feb1a09a9592e66dc
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-30 21:50:44 +0530

    loading made bigger and to the top of the page

--- app/page.tsx
+++ app/page.tsx
@@ -64,8 +64,8 @@ export default function Home() {
           </div>
         </div>
       ) : (
-        <div className="flex flex-col justify-center items-center h-screen text-white">
-          Loading...
+        <div className="flex flex-col justify-center items-center text-white">
+          <h1 className='text-3xl'>Loading...</h1> 
         </div>
       )}
     </section>
