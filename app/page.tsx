69399d44541738241dbb342be26824506d3b53a8
P SIDHARTH <philkhanasidharth14@gmail.com>
2023-11-30 17:04:48 +0530

    firestore updation successful

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,43 @@
-'use client'
-import React, { useState, useEffect } from 'react';
-import { getDoc, collection, updateDoc, doc } from 'firebase/firestore';
-import firestore from '../firebase';
-
-// Define an interface for the attendee data
-interface AttendeeData {
-  firstName: string;
-  lastName: string;
-  email: string;
-  organisation: string;
-  role: string;
-  flag: boolean;
-}
+'use client';
+import React, { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { db } from '../firebase.js';
 
 export default function Home() {
   const [extractedEmail, setExtractedEmail] = useState<string | null>(null);
-  
-  useEffect(() => {
-    extractEmail();
-  }, []);
 
-  const extractEmail = async (): Promise<string | null> => {
+  const search = useSearchParams();
+
+  const extractEmail = async () => {
     try {
-      let currentURL = window.location.href;
-      let queryString = currentURL.split('?')[1];
+      const extractedEmail = search.get('email');
+      setExtractedEmail(extractedEmail);
 
-      if (queryString) {
-        let params = queryString.split('&');
+      if (extractedEmail) {
+        const doc = db.collection('attendees').doc(extractedEmail);
+        await doc.update({ flag: true });
 
-        for (let i = 0; i < params.length; i++) {
-          let param = params[i].split('=');
-
-          if (param[0] === 'gmail') {
-            let email = decodeURIComponent(param[1]);
-            alert('Extracted Email: ' + email);
-            setExtractedEmail(email);
-
-            // Automatically update the flag when the email is extracted
-            await updateFlag(email);
-
-            return email;
-          }
-        }
+        // Since the update method doesn't return a value, you can assume success if it doesn't throw an error
+        alert('Success');
+      } else {
+        alert('Email not found');
       }
-
-      alert('No email in the URL');
-      setExtractedEmail(null);
-      return null;
-    } catch (error) {
-      console.error('Error extracting and updating email:', error);
-      return null;
-    }
-  };
-
-  const handleConfirmClick = () => {
-    // Check if extractedEmail exists before calling updateFlag
-    if (extractedEmail) {
-      updateFlag(extractedEmail);
-      console.log('button clicked');
-      
-    }
-    console.log('extracted email doesnt exist');
-    
-  };
-
-  const updateFlag = async (email: string): Promise<void> => {
-    try {
-      const docRef = doc(firestore, 'attendees', email);
-
-      // Update the 'flag' field to true
-      await updateDoc(docRef, {
-        flag: true,
-      });
-
-      alert('Flag updated successfully for email: ' + email);
-    } catch (error) {
-      console.error('Error updating flag in Firestore:', error);
+    } catch (e) {
+      alert(`Something went wrong, please try again. ${e}`);
     }
   };
 
   return (
     <section>
-      <div className='flex justify-center items-center'>
-      <img
-        src="/devwhite.png"
-        alt="DevFest"
-        className="md:w-1/2 md:h-1/2 w-auto h-auto object-cover my-12"
-      />
+      <div className="flex justify-center items-center">
+        <img
+          src="/devwhite.png"
+          alt="DevFest"
+          className="md:w-1/2 md:h-1/2 w-auto h-auto object-cover my-12"
+        />
       </div>
       <div
-        className="bg-gradient-to-b from-blue-950 to-blue-600 
+        className="bg-gradient-to-b from-yellow-900 to-yellow-600 
       text-white font-sans flex flex-col justify-center items-center min-h-fit"
       >
         <div className="container mx-auto p-4 flex items-center justify-center flex-col">
@@ -102,10 +50,12 @@ export default function Home() {
         </div>
 
         <div
-          className="bg-white text-blue-600 my-16 p-4 rounded-full hover:bg-white/80 cursor-pointer transition duration-200"
-          onClick={handleConfirmClick}
+          className="bg-white text-yellow-600 my-16 p-4 rounded-full hover:bg-white/80 cursor-pointer transition duration-200"
+          onClick={extractEmail}
         >
-          <h1 className="text-2xl font-bold px-4">Confirm your participation</h1>
+          <h1 className="text-2xl font-bold px-4">
+            Confirm your participation
+          </h1>
         </div>
       </div>
 
